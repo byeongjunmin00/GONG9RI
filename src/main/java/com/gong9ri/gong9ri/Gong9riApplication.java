4a4b3f7def226c21fc1258b5126ab64a724d41d0
minbyeongjun <byeongjunmin00@gmail.com>
2026-07-21 15:36:12 +0900

    chore: 프로젝트 초기 설정
    - Spring Boot 프로젝트 기본 구조 생성 (build.gradle, gradle wrapper)
    - MySQL 연결 설정 (application.yaml)

---
+++ src/main/java/com/gong9ri/gong9ri/Gong9riApplication.java
@@ -0,0 +1,12 @@
+package com.gong9ri.gong9ri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gong9riApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gong9riApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gong9ri/gong9ri/Gong9riApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gong9ri.gong9ri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gong9riApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
